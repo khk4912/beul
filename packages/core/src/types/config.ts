8d7f912e22d39be997da1f12df4daab18fcc764c
khk4912 <kosame@kosame.dev>
2026-02-11 18:38:48 +0900

    🏷️ feat: Add type for BeulConfig

---
+++ packages/core/src/types/config.ts
@@ -0,0 +1,8 @@
+export interface BeulConfig {
+  siteTitle?: string; // default: 'Beul SSG'
+  description?: string; // default: 'A blog powered by Beul'
+  baseURL?: string // default: '/'
+  contentDir?: string // default: 'contents'
+  outDir?: string // default : 'dist'
+  theme?: string // default: '@beul-ssg/theme-default'
+}
