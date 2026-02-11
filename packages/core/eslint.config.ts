2384b0458f9c214c6abf97ab6fc25001634fe837
khk4912 <kosame@kosame.dev>
2026-02-11 15:32:13 +0900

    🔧 chore: Update eslint/tsc config not to include unnecessary files

--- packages/core/eslint.config.ts
+++ packages/core/eslint.config.ts
@@ -1,10 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import neostandard from 'neostandard'
 
 export default defineConfig([
+  globalIgnores(['**/dist/**', '**/node_modules/**']),
   ...neostandard({
     ts: true,
     filesTs: ['**/*.{ts,tsx}'],
@@ -13,6 +14,9 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     plugins: { js },
     extends: ['js/recommended'],
+  },
+  tseslint.configs.recommendedTypeChecked,
+  {
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -20,6 +24,5 @@ export default defineConfig([
         tsconfigRootDir: __dirname,
       },
     },
-  },
-  tseslint.configs.recommendedTypeChecked,
+  }
 ])
